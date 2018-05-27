b185db6e28a0c52817f0b2e64fe227af46c0db32
Rohan awad <rohan@dream11.com>
2018-05-27 16:16:00 +0530

    logic to run app as interactive of file input

---
+++ src/execmode.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+const readline = require("readline");
+
+/*
+* Reads the command line argument for an input file.
+* If present, returns a file stream for that file else returns CLI.
+* */
+exports.setupExecMode = () => readline.createInterface({ input: process.argv[2] ? fs.createReadStream(process.argv[2]) : process.stdin, output: process.stdout, terminal: false });
