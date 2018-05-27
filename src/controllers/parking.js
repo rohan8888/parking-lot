af607b57849dcc5f12fa4c6f52a3740e3aa4251d
Rohan awad <rohan@dream11.com>
2018-05-27 19:22:38 +0530

    stable create park leave status

--- src/controllers/parking.js
+++ src/controllers/parking.js
@@ -8,8 +8,9 @@ const p = {};
 /*
 * Initializes the parking lot with given size
 * */
-const createParkingLot = function(size){
-    p["slots"] = Array.apply(null,  new Array(size)).map(i => 0);
+const createParkingLot = function (size) {
+    size = parseInt(size);
+    p["slots"] = Array.apply(null, new Array(size)).map(i => 0);
     console.log(`Created a parking lot with ${size} slots`);
 };
 
@@ -18,19 +19,31 @@ const getAvailableSlot = (slots) => slots.indexOf(0);
 /*
 * Find nearest available slot and assign a car
 * */
-const park = function(reg, color){
-    let slot =  getAvailableSlot(p.slots);
-    if(slot === -1) return console.log("Sorry, parking lot is full");
+const park = function (reg, color) {
+    let slot = getAvailableSlot(p.slots);
+    if (slot === -1) return console.log("Sorry, parking lot is full");
 
-    p.slots[slot] = { "regn": reg, "color": color };
+    p.slots[slot] = {"regn": reg, "color": color};
+    console.log(`Allocated solt number: ${slot+1}`);
+};
+
+const printCar = function(c, i){
+    if(c !== 0){
+        console.log(`${i+1}\t${c.regn}\t${c.color}`)
+    }
 };
 
 /*
 * Prints all cars in parking lot
 * */
-const status = function(){
+const status = function () {
     console.log(`Slot\tRegistration\tColor`);
-    p.slots.forEach( (c, i) => console.log(`${i}\t${c.regn}\t${c.color}`));
+    p.slots.forEach(printCar);
+};
+
+const leave = function (slot) {
+    p.slots[slot - 1] = 0;
+    console.log(`Slot number ${slot} is free`);
 };
 
 /*
@@ -39,7 +52,8 @@ const status = function(){
 const actionHandler = {
     "create_parking_lot": createParkingLot,
     "park": park,
-    "status": status
+    "status": status,
+    "leave": leave
 };
 
 exports.issueCommand = (input) => actionHandler[U.getAction(input)](...U.getAttributes(input));
