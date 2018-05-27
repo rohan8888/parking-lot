0f6acb334aca797e1345cf52be151692eb0eb3ff
Rohan awad <rohan@dream11.com>
2018-05-27 18:54:33 +0530

    status function

--- src/controllers/parking.js
+++ src/controllers/parking.js
@@ -9,15 +9,37 @@ const p = {};
 * Initializes the parking lot with given size
 * */
 const createParkingLot = function(size){
-    p["slots"] = new Array(size);
+    p["slots"] = Array.apply(null,  new Array(size)).map(i => 0);
     console.log(`Created a parking lot with ${size} slots`);
+};
+
+const getAvailableSlot = (slots) => slots.indexOf(0);
+
+/*
+* Find nearest available slot and assign a car
+* */
+const park = function(reg, color){
+    let slot =  getAvailableSlot(p.slots);
+    if(slot === -1) return console.log("Sorry, parking lot is full");
+
+    p.slots[slot] = { "regn": reg, "color": color };
+};
+
+/*
+* Prints all cars in parking lot
+* */
+const status = function(){
+    console.log(`Slot\tRegistration\tColor`);
+    p.slots.forEach( (c, i) => console.log(`${i}\t${c.regn}\t${c.color}`));
 };
 
 /*
 * An action handler with all allowed actions which mutate or read the parking object
 * */
 const actionHandler = {
-    "create_parking_lot": createParkingLot
+    "create_parking_lot": createParkingLot,
+    "park": park,
+    "status": status
 };
 
 exports.issueCommand = (input) => actionHandler[U.getAction(input)](...U.getAttributes(input));
